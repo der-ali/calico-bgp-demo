5f9c490e7104f922b31d196c271e721e4951b9ff
Ali, Akil <ali-akil@gmx.de>
2023-06-27 15:26:11 +0200

    refactor looping logic

--- setup.sh
+++ setup.sh
@@ -24,37 +24,39 @@ clusters[cluster2]="10.210.0.0/16 10.211.0.0/16"
 
 
 provision_minikube() {
-  for cluster in "${!clusters[@]}"; do
-    minikube -p $cluster start  --extra-config=kubeadm.pod-network-cidr=${clusters[$key]%% *} --service-cluster-ip-range=${clusters[$key]#* } --network=calico_cluster_peer_demo --container-runtime=containerd --nodes 4 --driver=kvm --memory 2048 --wait=all
-  done
+  local cluster=$1
+  local pod_network_cidr=$2
+  local service_cluster_ip_range=$3
+    minikube -p $cluster start  --extra-config=kubeadm.pod-network-cidr=${pod_network_cidr} --service-cluster-ip-range=${service_cluster_ip_range} --network=calico_cluster_peer_demo --container-runtime=containerd --nodes 4 --driver=kvm --memory 2048 --wait=all
 }
 
 
 # Function to check if a Kubernetes node is healthy
 check_node_health() {
-    local node=$1
-    local cluster=$2
-    local health_status=$(kubectl --cluster=$cluster get node $node -o jsonpath='{range @.status.conditions[-1:]}{.status}{end}')
+  local node=$1
+  local cluster=$2
+  local health_status=$(kubectl --cluster=$cluster get node $node -o jsonpath='{range @.status.conditions[-1:]}{.status}{end}')
 
-    if [[ "$health_status" != "True" ]]; then
-        echo "Node $node is not healthy!"
-        exit 1
-    else
-        echo "Node $node is healthy."
-    fi
+  if [[ "$health_status" != "True" ]]; then
+      echo "Node $node is not healthy!"
+      exit 1
+  else
+      echo "Node $node is healthy."
+  fi
 }
 
 check_nodes_health() {
   # Loop through each node and check its health
-  for cluster in "${!clusters[@]}"; do
-    local nodes=$(get_nodes ${cluster})
-    for node in $nodes; do
-      check_node_health ${node} ${cluster}
-    done
+  local cluster=$1
+  local nodes=$(get_nodes ${cluster})
+
+  for node in $nodes; do
+    check_node_health ${node} ${cluster}
   done
 }
 
 setup_cni() {
+  local cluster=$1
   kubectl --cluster=$cluster create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml 2> /dev/null
   kubectl --cluster=$cluster create -f ${cluster}_calicomanifests/custom-resources.yaml 2> /dev/null
   kubectl --cluster=$cluster delete ds -n=kube-system kindnet
@@ -65,25 +67,32 @@ setup_cni() {
 }
 
 install_calicoctl() {
-  for node in ${NODES}; do
+  local cluster=$1
+  local nodes=$(get_nodes ${cluster})
+  for node in ${nodes}; do
     minikube ssh -p $cluster -n $node "curl -o calicoctl -O -L http://github.com/projectcalico/calicoctl/releases/download/v3.21.4/calicoctl && sudo mv calicoctl /usr/bin/calicoctl && chmod +x /usr/bin/calicoctl"
   done
 }
 
 check_bgp_status() {
-  for node in ${NODES}; do
+  local cluster=$1
+  local nodes=$(get_nodes ${cluster})
+  for node in ${nodes}; do
     minikube ssh -p $cluster -n $node "sudo calicoctl node status"
   done
 
 }
 
 apply_bgp_config() {
+  local cluster=$1
   kubectl config use-context $cluster && calicoctl apply -f ${cluster}_calicomanifests/bgp-configuration.yaml
 }
 
 
 apply_bgp_rr_config() {
-  for node in ${RR_NODES}; do
+  local cluster=$1
+  local rr_nodes=$(get_rr_nodes ${cluster})
+  for node in ${rr_nodes}; do
     kubectl config use-context $cluster 
     calicoctl apply -f ${cluster}_calicomanifests/bgp-configuration.yaml
     #kubectl --cluster=$cluster drain --ignore-daemonsets $node
@@ -97,14 +106,23 @@ apply_bgp_rr_config() {
 }
 
 get_nodes() {
-      local cluster=$1
-      kubectl --cluster=$cluster get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+  local cluster=$1
+  kubectl --cluster=$cluster get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
 }
 
-provision_minikube
-#RR_NODES=$(kubectl --cluster=$cluster get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'| tail -2)
-check_nodes_health
-#setup_cni
-#install_calicoctl
-#apply_bgp_rr_config
+get_rr_nodes() {
+  local cluster=$1
+  kubectl --cluster=$cluster get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'| tail -2
+}
+
+for cluster in "${!clusters[@]}"; do
+  pod_network_cidr=${clusters[$cluster]%% *}
+  service_cluster_ip_range=${clusters[$cluster]#* }
+  provision_minikube $cluster ${pod_network_cidr} ${service_cluster_ip_range}
+  check_nodes_health $cluster
+  setup_cni $cluster
+  install_calicoctl $cluster
+  check_bgp_status $cluster
+  apply_bgp_rr_config
+done
 
